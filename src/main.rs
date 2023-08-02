e9850755c041651cbdd3e6ccc872ccbe6d6b7f7c
Hubert Lefevre <hubelefevre@gmail.com>
2023-08-02 23:59:59 +0200

    Hello world using git2

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use git2::Repository;
+use std::env;
+
+fn get_current_working_dir() -> String {
+    let res = env::current_dir();
+    match res {
+        Ok(path) => path.into_os_string().into_string().unwrap(),
+        Err(_) => "FAILED".to_string()
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cwd = get_current_working_dir();
+    let repo = match Repository::init(cwd) {
+        Ok(repo) => repo,
+        Err(e) => panic!("failed to init: {}", e),
+    };
+
+
+    let head = match repo.head() {
+        Ok(head) => head,
+        Err(e) => panic!("failed to get head: {}", e),
+    };
+
+    let shortname = match head.shorthand() {
+        Some(shortname) => shortname,
+        None => panic!("failed to get head shortname"),
+    };
+
+    println!("Hello, world! HEAD shortname is {}", shortname);
 }
